83ef3fec087df6e53785663ef6c889caa1483d8f
ngan <vutran@sheridancollege.ca>
2024-03-17 12:00:31 -0400

    Render Cards from data.js file

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,57 +1,61 @@
-import { AddComment } from "./AddComment";
 import { CommentCard } from "./CommentCard";
 import { Dialog, Transition } from "@headlessui/react";
 import { useState, Fragment } from "react";
 import { Outlet } from "react-router-dom";
+import {allComments} from "../data"
 
 export function CommentSection() {
   let [isOpen, setIsOpen] = useState(false);
+
+  let cards = []
+  allComments.forEach(obj => {
+    if(obj.replies.length) {
+      let cardRendered = 
+        <CommentCard
+          key={obj.id}
+          openDialog={setIsOpen}
+          commentOwnerName={obj.user.username || "Anonymous"}
+          createdAt={obj.createdAt}
+          commentLikeNumber={obj.likes}
+          commentOwnerImage={obj.user.image || "No Image"}
+          commentContent={obj.content}
+        >
+          {
+            obj.replies.map(replies => 
+              <CommentCard
+                key={replies.id}
+                openDialog={setIsOpen}
+                commentOwnerName={replies.user.username || "Anonymous"}
+                createdAt={replies.createdAt}
+                commentLikeNumber={replies.likes}
+                commentOwnerImage={replies.user.image || "No Image"}
+                commentContent={replies.content}
+              />
+            )
+          }
+        </CommentCard>
+      cards.push(cardRendered)
+    } else {
+      let cardRendered = 
+        <CommentCard
+          key={obj.id}
+          openDialog={setIsOpen}
+          commentOwnerName={obj.user.username || "Anonymous"}
+          createdAt={obj.createdAt}
+          commentLikeNumber={obj.likes}
+          commentOwnerImage={obj.user.image || "No Image"}
+          commentContent={obj.content}
+        />
+      cards.push(cardRendered)
+    }
+  })
 
   return (
     <>
       <div className="bg-slate-200 p-4">
 
-        {/* Card does not have replies */}
-        <CommentCard
-          openDialog={setIsOpen}
-          commentOwnerName="amyrobson"
-          createdAt={new Date(2024, 1, 2, 12, 30, 0, 0).getTime()}
-          commentLikeNumber={12}
-          commentOwnerImage="image-amyrobson.png"
-          commentContent="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed the design and the responsiveness at various breakpoints works really well."
-        />
+        {cards}
 
-        {/* Card has replies */}
-        <CommentCard
-          openDialog={(v) => setIsOpen(v)}
-          commentOwnerName="maxblagun"
-          commentOwnerImage="image-maxblagun.png"
-          createdAt={new Date(2024, 1, 25, 12, 30, 0, 0).getTime()}
-          commentLikeNumber={5}
-          commentContent="Woah, your project looks awesome! How long have you been coding for? I’m still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!"
-        >
-          <CommentCard
-            openDialog={setIsOpen}
-            referToOthers={"maxblagun"}
-            commentOwnerName="ramsesmiron"
-            commentOwnerImage="image-ramsesmiron.png"
-            createdAt={new Date(2024, 2, 6, 12, 30, 0, 0).getTime()}
-            commentLikeNumber={4}
-            commentContent="If you’re still new, I’d recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It’s very tempting to jump ahead but lay a solid foundation first."
-          />
-          <CommentCard
-            openDialog={setIsOpen}
-            referToOthers={"amyrobson"}
-            isYou={true}
-            commentOwnerName={"fcbvttn"}
-            commentOwnerImage="image-juliusomo.png"
-            createdAt={new Date(2024, 2, 11, 12, 30, 0, 0).getTime()}
-            commentLikeNumber={2}
-            commentContent="I couldn’t agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant."
-          />
-        </CommentCard>
-
-        {/* <AddComment avt="image-juliusomo.png" /> */}
         <Outlet />
 
       </div>
---
+++ src/data.js
@@ -0,0 +1,48 @@
+export let allComments = [
+
+  {
+      id: 1,
+      content: "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+      createdAt: new Date(2024, 1, 2, 12, 30, 0, 0).getTime(),
+      likes: 12,
+      user: {
+          image: "image-amyrobson.png",
+          username: "amyrobson"
+      },
+      replies: []
+  }, 
+
+  {
+      id: 2,
+      content: "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+      createdAt: new Date(2024, 1, 25, 12, 30, 0, 0).getTime(),
+      likes: 5,
+      user: {
+        image: "image-maxblagun.png",
+        username: "maxblagun"
+      },
+      replies: [
+        {
+          id: 3,
+          content: "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+          createdAt: new Date(2024, 2, 6, 12, 30, 0, 0).getTime(),
+          likes: 4,
+          user: {
+            image:  "image-ramsesmiron.png",
+            username: "ramsesmiron"
+          }
+        },
+        {
+          id: 4,
+          content: "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+          createdAt: new Date(2024, 2, 11, 12, 30, 0, 0).getTime(),
+          likes: 2,
+          user: {
+            image:  "image-juliusomo.png",
+            username: "juliusomo"
+          }
+        }
+      ]
+    }
+
+]
