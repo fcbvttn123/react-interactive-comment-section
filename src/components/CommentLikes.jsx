b701796bb5eb54df51f01f45ef8b15fcd439e48b
ngan <vutran@sheridancollege.ca>
2024-04-22 12:54:38 -0400

    The 'CommentLikes' component is updated

--- src/components/CommentLikes.jsx
+++ src/components/CommentLikes.jsx
@@ -1,29 +1,34 @@
 import { HeartIcon } from '@heroicons/react/24/solid'
 import { useRef, useState } from 'react'
 
+// Including the "Heart Icon" and the "Like Amount Number" of the "Comment Card"
 export function CommentLikes({likeNumbers=0}) {
 
     const [liked, setLike] = useState(false)
-    const refData_LikeNumbers = useRef(likeNumbers)
+    const currentLikeNumbers = useRef(likeNumbers)
 
     // Style for "like and unlike status" 
     let unlikeStyle = {fill: "white", stroke: "black"}
     let likeStyle = {fill: "rgb(239 68 68)", stroke: "rgb(239 68 68)"}
 
-    function toggleLike() {
-        // Update "like numbers" 
-        liked ? --refData_LikeNumbers.current : ++refData_LikeNumbers.current 
-        // Toggle "like state" status 
+    // When a click event triggered on the "Heart Icon"
+        // --> The "Like Number" will be incremented or decremented by 1 
+        // --> The "Heart" will change from white to red or vice versa 
+    function toggleHeartIcon() {
+        liked ? --currentLikeNumbers.current : ++currentLikeNumbers.current 
         setLike(!liked)
     }
 
     return (
+        // Container (Flex)
         <div className='flex items-center'>
-            <button onClick={toggleLike}>
+            {/* Flex Item 1 --> Heart Icon (Button) */}
+            <button onClick={toggleHeartIcon}>
                 <span className="sr-only">Like-comment Button</span>
                 <HeartIcon className='w-6 h-5' style={liked ? likeStyle : unlikeStyle} aria-hidden="true"/>
             </button>
-            <span>{refData_LikeNumbers.current}</span>
+            {/* Flex Item 2 --> Number */}
+            <span>{currentLikeNumbers.current}</span>
         </div>
     )
 
