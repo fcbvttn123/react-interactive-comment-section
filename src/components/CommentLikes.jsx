8b3ce286fee497f46ff121e5f9b1dbbe99c2b8b9
ngan <vutran@sheridancollege.ca>
2024-04-23 18:49:07 -0400

    CommentLikes component updated.
    Move this line to a new useEffect() function: liked ? --currentLikeNumbers.current : ++currentLikeNumbers.current
    
    toggleHeartIcon() function is removed

--- src/components/CommentLikes.jsx
+++ src/components/CommentLikes.jsx
@@ -1,29 +1,32 @@
 import { HeartIcon } from '@heroicons/react/24/solid'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 // Including the "Heart Icon" and the "Like Amount Number" of the "Comment Card"
 export function CommentLikes({likeNumbers=0}) {
 
     const [liked, setLike] = useState(false)
     const currentLikeNumbers = useRef(likeNumbers)
+    const firstRender = useRef(true)
 
     // Style for "like and unlike status" 
     let unlikeStyle = {fill: "white", stroke: "black"}
     let likeStyle = {fill: "rgb(239 68 68)", stroke: "rgb(239 68 68)"}
 
-    // When a click event triggered on the "Heart Icon"
-        // --> The "Like Number" will be incremented or decremented by 1 
-        // --> The "Heart" will change from white to red or vice versa 
-    function toggleHeartIcon() {
-        liked ? --currentLikeNumbers.current : ++currentLikeNumbers.current 
-        setLike(!liked)
-    }
+    // After a "Click Event" (one render) on the "Heart Icon", the "Like Number" will be incremented or decremented by 1
+    // This useEffect() function is not triggered in the first component render 
+    useEffect(() => {
+        if(firstRender.current) {
+            firstRender.current = false
+        } else {
+            liked ? --currentLikeNumbers.current : ++currentLikeNumbers.current 
+        }
+    }, [liked])
 
     return (
         // Container (Flex)
         <div className='flex items-center'>
             {/* Flex Item 1 --> Heart Icon (Button) */}
-            <button onClick={toggleHeartIcon}>
+            <button onClick={() => setLike(!liked)}>
                 <span className="sr-only">Like-comment Button</span>
                 <HeartIcon className='w-6 h-5' style={liked ? likeStyle : unlikeStyle} aria-hidden="true"/>
             </button>
